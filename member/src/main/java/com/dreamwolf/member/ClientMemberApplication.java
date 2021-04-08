7aa6830ce35d7132fd08282da703a5308e47f22f
DESKTOP-PU318SS\shinelon <1828558701@qq.com>
2021-04-08 10:48:54 +0800

    创建 -- 用户模块 User  userData  Relations  VIP -- 分区模块 Zoning   zoningRelation -- 用户动态模块 userDynamic  dynamicData  dynamicLike  dynamicComment

---
+++ member/src/main/java/com/dreamwolf/member/ClientMemberApplication.java
@@ -0,0 +1,11 @@
+package com.dreamwolf.member;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClientMemberApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ClientMemberApplication.class, args);
+    }
+}
