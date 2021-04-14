6775cebd63c40ef92f0d24c22a32387a16f3738e
BELONG-X <2904981788@qq.com>
2021-04-14 10:33:07 +0800

    收藏夹

--- fav/src/main/java/com/dreamwolf/fav/FavApplication.java
+++ fav/src/main/java/com/dreamwolf/fav/FavApplication.java
@@ -4,6 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
 
+/**
+ * a
+ */
 @RefreshScope
 @SpringBootApplication
 public class FavApplication {
