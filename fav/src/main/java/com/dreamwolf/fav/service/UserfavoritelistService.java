e49e4329e631a5ea9f8af9de0a517ad7a409719b
YuMingC233 <48896152+YuMingC233@users.noreply.github.com>
2021-04-14 12:16:46 +0800

    修改了分区表中不合逻辑的部分

--- fav/src/main/java/com/dreamwolf/fav/service/UserfavoritelistService.java
+++ fav/src/main/java/com/dreamwolf/fav/service/UserfavoritelistService.java
@@ -1,16 +1,4 @@
 package com.dreamwolf.fav.service;
-
-import com.dreamwolf.fav.pojo.Userfavoritelist;
-import com.baomidou.mybatisplus.extension.service.IService;
-
-/**
- * <p>
- * 用户收藏列表 服务类
- * </p>
- *
- * @author 老徐
- * @since 2021-04-12
- */
-public interface UserfavoritelistService extends IService<Userfavoritelist> {
-
+//用户收藏列表
+public interface UserFavoriteListService {
 }
--- fav/src/main/java/com/dreamwolf/fav/service/impl/FavoritelikeServiceImpl.java
+++ fav/src/main/java/com/dreamwolf/fav/service/impl/FavoritelikeServiceImpl.java
@@ -1,20 +1,11 @@
 package com.dreamwolf.fav.service.impl;
 
-import com.dreamwolf.fav.pojo.Favoritelike;
-import com.dreamwolf.fav.mapper.FavoritelikeMapper;
-import com.dreamwolf.fav.service.FavoritelikeService;
-import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
-import org.springframework.stereotype.Service;
-
-/**
- * <p>
- *  服务实现类
- * </p>
- *
- * @author 老徐
- * @since 2021-04-12
- */
-@Service
-public class FavoritelikeServiceImpl extends ServiceImpl<FavoritelikeMapper, Favoritelike> implements FavoritelikeService {
+///**
+// * @author: wzx
+// * @data: 2021/4/7 9:42
+// * @version: 1.0
+// */
+//收藏夹状态表
+public class FavoriteLikeServiceImpl {
 
 }
